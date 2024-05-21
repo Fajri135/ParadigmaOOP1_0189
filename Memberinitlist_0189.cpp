2c67f6c9432b61e2b0df63201ed69c4920c98927
Fajri135 <corex135@gmail.com>
2024-05-21 11:45:55 +0700

    Menambahkan destruktor

--- Memberinitlist_0189.cpp
+++ Memberinitlist_0189.cpp
@@ -14,6 +14,11 @@ public:
 	mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai) {
 	}
 
+	~mahasiswa() {
+		cout << "id		= " << id << endl;
+		cout << "Nama   = " << nama << endl;
+		cout << "Nilai  + " << nilai << endl;
+	}
 };
 
 int main() {
