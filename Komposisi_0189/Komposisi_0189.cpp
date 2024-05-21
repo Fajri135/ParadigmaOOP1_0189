beb2f5ca0765ad69a8504b858c317028e2e71b15
Fajri135 <corex135@gmail.com>
2024-05-21 11:48:22 +0700

    Membuat file header iostream dan string serta menggunakan namespace standar C++

--- Komposisi_0189/Komposisi_0189.cpp
+++ Komposisi_0189/Komposisi_0189.cpp
@@ -2,12 +2,8 @@
 #include <string>
 using namespace std;
 
-#include "Jantung.h"
-#include "Manusia.h"
 
 int main() {
-    manusia* varManusia = new manusia("Jono");
-    delete varManusia;
-    return 0;
+   ;
 }
 
