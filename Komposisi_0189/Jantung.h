d450d6106ab907ac737b77fe1b1f9fa717599f8e
Fajri135 <corex135@gmail.com>
2024-05-21 11:50:30 +0700

    Membuat header guards Jantung

--- Komposisi_0189/Jantung.h
+++ Komposisi_0189/Jantung.h
@@ -1,14 +1,6 @@
 #ifndef JANTUNG_H
 #define JANTUNG_H
 
-class jantung {
-public:
-	jantung() {
-		cout << "Jantung dihidupkan\n";
-	}
-	~jantung() {
-		cout << "Jantung dimatikan\n";
-	}
-};
+
 
 #endif 
--- Komposisi_0189/Komposisi_0189.cpp
+++ Komposisi_0189/Komposisi_0189.cpp
@@ -2,8 +2,12 @@
 #include <string>
 using namespace std;
 
+#include "Jantung.h"
+#include "Manusia.h"
 
 int main() {
-   ;
+    manusia* varManusia = new manusia("Jono");
+    delete varManusia;
+    return 0;
 }
 
